67192d081ac0f9bbf4ec75bba6f864db20360b49
neawest <nea.westerling@gmail.com>
2022-10-13 13:09:01 +0300

    Initial Commit

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { MovieService } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+  movies = [];
+  currentPage = 1;
+
+  constructor(private movieService: MovieService, private loadingCtrl: LoadingController) {}
+
+  ngOnInit() {
+    this.loadMovies();
+  }
+
+  async loadMovies() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading..',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+
+    this.movieService.getTopRatedMovies(this.currentPage).subscribe(
+      (res) => {
+      loading.dismiss();
+      this.movies = [...this.movies, ...res.results];
+      console.log(res);
+    });
+  }
+
+}
